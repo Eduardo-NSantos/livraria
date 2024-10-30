ec6e596d9bf9b20829454551321141f075241f3d
Eduardo-SNantos <eduardoshw123@gmail.com>
2024-10-29 21:06:52 -0300

    adicionei a funcionalidade de pesquisa

--- users/index.php
+++ users/index.php
@@ -1,5 +1,4 @@
-<?php 
-    require "sessao.php";
+<?php
     require "../conexao.php";
 ?>
 <!DOCTYPE html>
@@ -20,35 +19,50 @@
 </head>
 <body>
     <h1>Livraria Online</h1>
-    <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-        <input type="search" name="busca" id="busca">
-        <input type="submit" value="pesquisar">
-    </form>
+    <main>
+        <nav>
+            <a href="#">pesquisar por autor</a><br>
+            <a href="#">Pesquisar por gênero</a><br>
+            <a href="#">Pesquisar por editora</a><br>
+        </nav>
 
-    <table>
-        <tr>
-            <th>Título</th>
-            <th>Autor</th>
-            <th>Gênero</th>
-            <th>Editora</th>
-        </tr>
-        <?php
-            $sql = "select * from livros";
-            $dados = mysqli_query($conexao, $sql);
-            
-            while($itens = mysqli_fetch_assoc($dados)){
-                $titulo = $itens['titulo'];
-                $autor = $itens['autor'];
-                $genero = $itens['genero'];
-                $editora = $itens['editora'];
-                echo "<tr>";
-                    echo "<th>$titulo</th>";
-                    echo "<td>$autor</td>";
-                    echo "<td>$genero</td>";
-                    echo "<td>$editora</td>";
-                echo "</tr>";
-            }
-        ?>
-    </table>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+            <input type="search" name="busca" id="busca" placeholder="Titulo do livro">
+            <input type="submit" value="pesquisar">
+        </form>
+        <table>
+            <tr>
+                <th>Título</th>
+                <th>Autor</th>
+                <th>Gênero</th>
+                <th>Editora</th>
+            </tr>
+        
+            <?php
+                $pesquisa = anti_injection($conexao, $_GET['busca'] ?? "");
+                $sql = "select * from livros where titulo like '%$pesquisa%'";
+                $dados = mysqli_query($conexao, $sql);
+        
+                if(mysqli_num_rows($dados) >= 1){
+                    while($itens = mysqli_fetch_assoc($dados)){
+                        $titulo = $itens['titulo'];
+                        $autor = $itens['autor'];
+                        $genero = $itens['genero'];
+                        $editora = $itens['editora'];
+                        echo "<tr>";
+                            echo "<th>$titulo</th>";
+                            echo "<td>$autor</td>";
+                            echo "<td>$genero</td>";
+                            echo "<td>$editora</td>";
+                        echo "</tr>";
+                    }
+                }else{
+                    echo "<tr>";
+                        echo "<th colspan='4'>Nenhum livro com esse título foi encontrado na base de dados, verifique a escrita e tente novamente</th>";
+                    echo "</tr>";
+                }
+            ?>
+        </table>
+    </main>
 </body>
 </html>
